634fa803f710cd5a6b662244c9946de32a085e01
Karthik Baskar <kabaska@microsoft.com>
2016-07-24 03:31:08 +0530

    Adding basic activities

--- HereWeGo/App/HereWeGo/app/src/main/java/com/microsoft/android/herewego/ui/HomeActivity.java
+++ HereWeGo/App/HereWeGo/app/src/main/java/com/microsoft/android/herewego/ui/HomeActivity.java
@@ -2,6 +2,7 @@ package com.microsoft.android.herewego.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.microsoft.android.herewego.R;
 
@@ -11,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+
     }
 }
