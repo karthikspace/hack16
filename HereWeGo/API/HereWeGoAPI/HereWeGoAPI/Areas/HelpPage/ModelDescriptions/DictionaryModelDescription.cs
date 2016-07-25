d2029e16ab10e84bad7ec81a3e277ea2ff93fed8
Abhishek Gupta (BING UX) <abgupt@microsoft.com>
2016-07-25 12:32:19 +0530

    New API

---
+++ HereWeGo/API/HereWeGoAPI/HereWeGoAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HereWeGoAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HereWeGo/API/HereWeGoAPI/HereWeGoAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HereWeGoAPI.WebApiApplication" Language="C#" %>
